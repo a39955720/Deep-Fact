4db59006db155e6325f0b4fa4285305901d8e93b
Denny Chen <a39955720@gmail.com>
2024-06-29 17:46:52 +0000

    update frontend

--- nextjs/constants/index.js
+++ nextjs/constants/index.js
@@ -1,6 +1,6 @@
 const DeepFactAbi = require("./DeepFactAbi.json")
 const ZircuitContractAddress = "0x003320742a1D147bcF1E987a3E85dD6d8B0d0330"
-const OptimismContractAddress = "0x092D1b3961B566aee15ecf01eda22D366Cb66b8c"
+const OptimismContractAddress = "0x8F2D19295a2De63EBbB2906246Ed6d52e49E6bac"
 
 module.exports = {
     DeepFactAbi,
