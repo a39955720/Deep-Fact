0dbddb0686af8723cb236b674590068ccb3dea8c
LIU-CHIA-DA <itri461370@itri.org.tw>
2024-06-29 15:08:15 +0800

    add href link

--- nextjs/components/Start.js
+++ nextjs/components/Start.js
@@ -14,7 +14,7 @@ export default function Start() {
             </div>
             <h2 className="mt-20 text-7xl font-bold mb-10" style={{ color: 'black' }}>How do you want to contribute?</h2>
             <div className="flex space-x-10 mt-10 w-full justify-center">
-                <Link href="/audit-and-earn" className="w-2/5">
+                <Link href="/audit" className="w-2/5">
                     <div className="gradient-flow text-white p-18 rounded-lg shadow-md hover:shadow-lg transition duration-300 flex flex-col justify-center items-center" style={{ minHeight: "500px" }}>
                         <h3 className="custom-link text-4xl font-bold mb-4 hover:text-blue-500 transition duration-300">Want to Help others?</h3>
                         <p className=" text-lg font-bold  transition duration-300">Answer questions and Start to Earn ! →</p>
